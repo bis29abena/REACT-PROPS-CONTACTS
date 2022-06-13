03be2f71695e0ea1804ad0ec6df846b07571aacd
Bismark Osei <bismarkoseiabena@gmail.com>
2022-06-13 23:48:16 +0000

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div>
+      <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <img src={props.image} alt="avatar_img" className="circle-img" />
+        </div>
+        <div className="bottom">
+          <p>{props.phone}</p>
+          <p>{props.email}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
